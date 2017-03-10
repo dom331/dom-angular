63c949e5a878ca082d4fdcb50f5927a2436b56a7
Splitziz <nudoru70@gmail.com>
2017-03-10 02:36:02 +0100

    Changement page vie scolaire + inscriptions

--- api/pages/viescolaire/objetsperdus.php
+++ api/pages/viescolaire/objetsperdus.php
@@ -1,42 +1,26 @@
 <?php
 
-require_once '../../dao/DaoActualite.php';
+require_once '../../dao/DaoObjets_perdus.php';
 //var_dump($_SESSION) or die();
-$dao = new DaoActualite();
+$dao = new DaoObjets_perdus();
 
-$liste = $dao->getListePerdu();
+$liste = $dao->getListe();
 
 for($i=0;$i<count($liste); $i++){
 
-    $dao = new DaoActualite();
+    $dao = new DaoObjets_perdus();
 
     $dao->find($liste[$i]->getId());
+
 
     $liste[$i] = $dao->bean;
 
 }
 
-//var_dump($param) or die();
-
-require_once('../../dao/DaoUtilisateur.php');
-require_once('../../dao/DaoEvenements.php');
-$daoU = new DaoUtilisateur();
-
-$liste2 = $daoU->getNonApprouve();
-
-$users = count($liste2);
-
-$daoE = new DaoEvenements();
-
-$liste3 = $daoE->listeAprob();
-
-$events = count($liste3);
-
-$notifs['number'] = array();
-$notifs['number']['oui'] = $events + $users;
 $param = array(
-    "liste" => $liste,
-    "notifs" => $notifs);
+    "objets" => $liste);
 
+echo json_encode($param);
+//var_dump($param) or die();
 
 ?>
