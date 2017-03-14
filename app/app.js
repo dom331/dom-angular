3520ac3ab7e8fb820bc58dcba7a7653e0b59d1e1
Splitziz <nudoru70@gmail.com>
2017-03-15 00:17:22 +0100

    Application REGEX

--- app/app.js
+++ app/app.js
@@ -1,6 +1,6 @@
 // On crée l'application "app" et on utilise une injection "ngRoute" pour l'url
 
-var app = angular.module('app', ['ngRoute', 'ngFileUpload']);
+var app = angular.module('app', ['ngRoute', 'ngFileUpload', 'ngMessages']);
 
 app.config(function ($httpProvider) {
     $httpProvider.defaults.headers.common = {};
